bce1b69c89fe61aa9a84ecef25e3eab70e15bf21
kraskoo <kraskoo87@gmail.com>
2019-11-09 18:17:52 +0200

    Update usage of string-builder

--- 01_StringBuilder/TestStringBuilder/TestStringBuilder.cpp
+++ 01_StringBuilder/TestStringBuilder/TestStringBuilder.cpp
@@ -7,9 +7,13 @@ using sb::string_builder;
 int main()  // NOLINT
 {
 	int print_count(0);
+	// no need of 'new' keyword anymore, because of new standard i guess
+	// string_builder sb = string_builder();
+	
+	// this is strange, but the most famous way, for constructor without parameters
 	string_builder sb;
 	sb.append("3dfsdfsf").append_new_line();
-    cout << "Print ---- " << ++print_count << " ----\n" << sb.str();
+	cout << "Print ---- " << ++print_count << " ----\n" << sb.str();
 	sb.append("56dfgghjk").append_new_line();
 	cout << "Print ---- " << ++print_count << " ----\n" << sb.str();
 	return 0;
