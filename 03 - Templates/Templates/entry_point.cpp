938c254868fd069c255f27ef4dd953792bd193f5
Krasimir Stefanov <kraskoo87@gmail.com>
2019-11-24 04:24:28 +0200

    Fix templates

--- 03 - Templates/Templates/entry_point.cpp
+++ 03 - Templates/Templates/entry_point.cpp
@@ -9,11 +9,11 @@ int main()
 	const int a = 10;
 	const int b = 5;
 	const string abra = "abra";
-	int int_arr[] = { 5, 2, 1, 7, 54, 9999, 1324, 4 };
-	auto arr_value = concat(&int_arr, abra);
+	const int int_arr[] = { 5, 2, 1, 7, 54, 9999, 1324, 4 };
 	cout << "min(10, 5) = " << min(a, b) << endl <<
-		"max(10, 5) = " << max(a, b) << endl;
+		"max(10, 5) = " << max(a, b) << endl <<
+		"max(10.453466, 5) = " << max<double>(10.453466, 5) << endl;
 	cout << "concat(10, \"abra\") = " << concat(a, abra) << endl <<
-		"concat(5, 2, 1, 7, \"abra\") = " << arr_value << endl;
+		"concat(5, 2, 1, 7, 54, 9999, 1324, 4, \"abra\") = " << concat(&int_arr, abra) << endl;
 	return 0;
 }
